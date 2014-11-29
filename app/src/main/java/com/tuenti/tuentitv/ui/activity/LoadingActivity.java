847c2f394adae6651669ab1974dff24833e38c50
pedrovgs <pedrovicente.gomez@gmail.com>
2014-11-29 21:39:31 +0100

    Start working with the loading activity final design

--- app/src/main/java/com/tuenti/tuentitv/ui/activity/LoadingActivity.java
+++ app/src/main/java/com/tuenti/tuentitv/ui/activity/LoadingActivity.java
@@ -3,6 +3,8 @@ package com.tuenti.tuentitv.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
+import butterknife.InjectView;
 import com.tuenti.tuentitv.R;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,9 +16,13 @@ public class LoadingActivity extends BaseActivity {
 
   private static final long LOADING_TIME_IN_MILLIS = 3000;
 
+  @InjectView(R.id.pb_loading) ProgressBar pb_loading;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     setContentView(R.layout.loading_activity);
     super.onCreate(savedInstanceState);
+    pb_loading.getIndeterminateDrawable()
+        .setColorFilter(0x32FFFFFF, android.graphics.PorterDuff.Mode.MULTIPLY);
 
     new Handler().postDelayed(new Runnable() {
       @Override public void run() {
@@ -27,8 +33,8 @@ public class LoadingActivity extends BaseActivity {
 
   private void startMainActivity() {
     Intent intent = new Intent(this, MainActivity.class);
-    startActivity(intent);
-    finish();
+    //startActivity(intent);
+    //finish();
   }
 
   @Override protected List getModules() {
