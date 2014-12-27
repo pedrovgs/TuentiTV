2996e62a99b720a88a58f0bf6bb918e47231dcac
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-27 22:17:29 +0100

    Add alpha animation to ShowImageActivity

--- app/src/main/java/com/github/pedrovgs/tuentitv/ui/activity/ShowImageActivity.java
+++ app/src/main/java/com/github/pedrovgs/tuentitv/ui/activity/ShowImageActivity.java
@@ -2,6 +2,7 @@ package com.github.pedrovgs.tuentitv.ui.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import butterknife.InjectView;
@@ -15,6 +16,8 @@ public class ShowImageActivity extends BaseActivity {
 
   public static final String IMAGE_URL_EXTRA = "image_url_extra";
 
+  private static final int ALPHA_ANIMATION_DURATION = 1000;
+
   @InjectView(R.id.iv_media_element) ImageView iv_media_element;
   @InjectView(R.id.pb_loading) ProgressBar pb_loading;
 
@@ -24,15 +27,32 @@ public class ShowImageActivity extends BaseActivity {
     String imageUrl = getImageUrlFromExtras();
     Picasso.with(this).load(imageUrl).into(iv_media_element, new Callback() {
       @Override public void onSuccess() {
-        int backgroundColor = getResources().getColor(R.color.third_color);
-        iv_media_element.setBackgroundColor(backgroundColor);
-        pb_loading.setVisibility(View.GONE);
+        animateImageView();
+        updateImageViewBackground();
+        hideLoading();
       }
 
       @Override public void onError() {
         finish();
       }
     });
+  }
+
+  private void animateImageView() {
+    AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1.0f);
+    alphaAnimation.setDuration(ALPHA_ANIMATION_DURATION);
+    alphaAnimation.setFillAfter(true);
+    iv_media_element.startAnimation(alphaAnimation);
+    iv_media_element.setVisibility(View.VISIBLE);
+  }
+
+  private void updateImageViewBackground() {
+    int backgroundColor = getResources().getColor(R.color.third_color);
+    iv_media_element.setBackgroundColor(backgroundColor);
+  }
+
+  private void hideLoading() {
+    pb_loading.setVisibility(View.GONE);
   }
 
   @Override protected List getModules() {
