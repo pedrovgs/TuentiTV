37505832f7e0895d9978495417ff7520473a2fd7
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-29 21:32:51 +0100

    Show notification using RecommendationService when DetailActivity is opened

--- app/src/main/java/com/github/pedrovgs/tuentitv/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/pedrovgs/tuentitv/ui/activity/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.github.pedrovgs.tuentitv.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.github.pedrovgs.tuentitv.R;
+import com.github.pedrovgs.tuentitv.recommendation.service.RecommendationService;
 import com.github.pedrovgs.tuentitv.ui.fragment.DetailFragment;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,6 +18,12 @@ public class DetailActivity extends BaseActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.detail_activity);
+    startRecommendationService();
+  }
+
+  private void startRecommendationService() {
+    Intent service = new Intent(this, RecommendationService.class);
+    startService(service);
   }
 
   @Override protected List getModules() {
