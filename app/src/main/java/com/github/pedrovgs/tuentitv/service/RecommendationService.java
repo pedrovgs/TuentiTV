254826d4f22d2f9324a1152d5503a4fb2ff213e6
pedrovgs <pedrovicente.gomez@gmail.com>
2014-12-29 19:46:16 +0100

    Start working with RecommendationService

---
+++ app/src/main/java/com/github/pedrovgs/tuentitv/service/RecommendationService.java
@@ -0,0 +1,33 @@
+package com.github.pedrovgs.tuentitv.service;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
+import com.github.pedrovgs.tuentitv.ui.activity.LoginActivity;
+
+/**
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class RecommendationService extends IntentService {
+
+  private static final int MAX_RECOMMENDATIONS = 3;
+  private static final String SERVICE_NAME = "RecommendationService";
+
+  public RecommendationService() {
+    super(SERVICE_NAME);
+  }
+
+  @Override protected void onHandleIntent(Intent intent) {
+
+  }
+
+  private PendingIntent getPendingIntent() {
+    Intent detailsIntent = new Intent(this, LoginActivity.class);
+    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+    stackBuilder.addParentStack(LoginActivity.class);
+    stackBuilder.addNextIntent(detailsIntent);
+    PendingIntent intent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+    return intent;
+  }
+}
